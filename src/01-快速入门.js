ffaaf144ddcf2c420a1b4c8888cc9831806f5402
konglong <1016361268@qq.com>
2022-06-09 10:07:08 +0800

    Koa的安装以及简单使用

---
+++ src/01-快速入门.js
@@ -0,0 +1,13 @@
+// 1. 导入 koa 包
+const Koa = require('Koa')
+// 2. 实例化 app 对象
+const app = new Koa()
+// 3. 编写中间件 中间件就是一个函数
+app.use((ctx) => {
+  // ctx 代表 http 请求的上下文
+  ctx.body = 'hello koa'
+})
+// 4. 启动服务，监听3000端口
+app.listen(3000, () => {
+  console.log(`server is running on http://localhost:3000`)
+})
