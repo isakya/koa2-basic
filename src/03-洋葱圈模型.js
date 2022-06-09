1ba34e7ad6d6a9110849a983923700be9f41c57e
konglong <1016361268@qq.com>
2022-06-09 10:29:25 +0800

    洋葱圈模型

---
+++ src/03-洋葱圈模型.js
@@ -0,0 +1,24 @@
+// 1. 导入 koa 包
+const Koa = require('Koa')
+// 2. 实例化 app 对象
+const app = new Koa()
+// 3. 编写中间件 中间件就是一个函数
+app.use((ctx, next) => {
+  console.log(1)
+  next()
+  console.log(2)
+})
+app.use((ctx, next) => {
+  console.log(3)
+  next()
+  console.log(4)
+})
+app.use(ctx => {
+  console.log(5)
+  ctx.body = '处理完成'
+  console.log(6)
+})
+// 4. 启动服务，监听3000端口
+app.listen(3000, () => {
+  console.log(`server is running on http://localhost:3000`)
+})
