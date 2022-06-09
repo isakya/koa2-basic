8acb16ca30df5900ab8ac5997bd9cad7f8730498
konglong <1016361268@qq.com>
2022-06-09 17:43:36 +0800

    解析params的键值对参数

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -6,23 +6,27 @@ const router = new Router({ prefix: '/users' })
 // 3.3 编写路由规则
 
 const db = [
-  { id: 1, name: 'zs', age: 20 },
+  { id: 1, name: 'zs', age: 10 },
   { id: 2, name: 'zs', age: 20 },
-  { id: 3, name: 'zs', age: 20 },
-  { id: 4, name: 'zs', age: 20 },
-  { id: 5, name: 'zs', age: 20 },
+  { id: 3, name: 'zs', age: 45 },
+  { id: 4, name: 'zs', age: 30 },
+  { id: 5, name: 'zs', age: 40 },
 ]
 
 // GET /uers ---- 获取所有用户的信息，返回一个数组
 router.get('/', (ctx) => {
-  ctx.body = db
+  // 通过 ctx.query 是 ctx.request.query 的代理 解析键值对参数
+  const { start = 0, end = 0 } = ctx.query
+  const res = db.filter((item) => {
+    return item.age >= start && item.age <= end
+  })
+  res.length === 0 ? ctx.throw(404) : (ctx.body = res)
 })
 
 // GET /users/:id ---- 根据 id 获取单个用户的信息，返回一个对象
 router.get('/:id', (ctx) => {
   // 解析 id 参数
   const id = ctx.params.id
-  console.log(id)
   const res = db.filter(item => item.id == id)
   if (!res[0]) ctx.throw(404)
   ctx.body = res[0]
