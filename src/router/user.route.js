8229a4ff9ad13d228f2751faf044e24ef89466c2
konglong <1016361268@qq.com>
2022-06-09 17:32:01 +0800

    解析params参数

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -2,12 +2,32 @@
 // 3.1 导入koa-router 包
 const Router = require('koa-router')
 // 3.2 实例化 router 对象
-const router = new Router()
+const router = new Router({ prefix: '/users' })
 // 3.3 编写路由规则
-router.get('/users', (ctx) => {
-  ctx.body = '这个是用户页'
+
+const db = [
+  { id: 1, name: 'zs', age: 20 },
+  { id: 2, name: 'zs', age: 20 },
+  { id: 3, name: 'zs', age: 20 },
+  { id: 4, name: 'zs', age: 20 },
+  { id: 5, name: 'zs', age: 20 },
+]
+
+// GET /uers ---- 获取所有用户的信息，返回一个数组
+router.get('/', (ctx) => {
+  ctx.body = db
 })
-router.post('/users', (ctx) => {
+
+// GET /users/:id ---- 根据 id 获取单个用户的信息，返回一个对象
+router.get('/:id', (ctx) => {
+  // 解析 id 参数
+  const id = ctx.params.id
+  console.log(id)
+  const res = db.filter(item => item.id == id)
+  if (!res[0]) ctx.throw(404)
+  ctx.body = res[0]
+})
+router.post('/', (ctx) => {
   ctx.body = '创建用户'
 })
 
